79c21856f9ad700a28782cfeb315f4fbfe54eea7
Aleksandr Konyuk <a.konyuk@samsung.com>
2013-02-18 17:42:53 +0200

    added Learn and trade

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -188,172 +188,193 @@ void mainwindow::on_start_learn_clicked()
 
 void mainwindow::on_start_trade_clicked()
 {
-    /*//нахождение всех необходимы коефициетвов осцилляторов для торговли
-     for (i=open_t;i<=close_t;i++){
-         mas_ao[i][0]=ao(mas_low,mas_high,i);
-         mas_ao[i][1]=ao(mas_low,mas_high,i-7);
-         mas_ao[i][2]=ao(mas_low,mas_high,i-14);
-         mas_ao[i][3]=ao(mas_low,mas_high,i-21);
-         //printf("a1= %f a2= %f a3= %f a4= %f i= %d\n",mas_ao[i][0],mas_ao[i][1],mas_ao[i][2],mas_ao[i][3],i);
-     }
-    //Торговля
-    FILE* output_t=fopen("output_t.txt","w+");
-    float profit=0;
-    open=true,close=false;
-    int c=0;
-    fprintf(output_t,
-            "==========================\nCoefficients: w1=%f w2=%f w3=%f w4=%f\n===========================\n",
-            w1_max,w2_max,w3_max,w4_max);
-    for (int i=open_t;i<=close_t;i++){
-                        d=mas_ao[i][0]*w1_max+mas_ao[i][1]*w2_max+mas_ao[i][2]*w3_max+mas_ao[i][3]*w4_max;
-                        if (open==true){
-                           if (d>0){
-                              c++;
-                              profit=profit-mas_open[i];
-                              open=false;
-                              close=true;
-                              fprintf(output_t,
-                                      "--------------------------------------------\nDeal id= %d:\nOpen number in a file of quotations %d %dy %dm %dd %dh open price= %f profit= %f\n",
-                                      c,i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_open[i],profit);
-                           }
-                        }
-                        if (close==true){
-                           if (d<0){
-                              profit=profit+mas_close[i];
-                              close=false;
-                              open=true;
-                              fprintf(output_t,
-                                      "Close number in a file of quotations %d %dy %dm %dd %dh close price= %f profit= %f\n",
-                                      i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_close[i],profit);
-                           }
-                        }
-                        if ((close==true)&&(i==close_l)){
-                            profit=profit+mas_close[i];
-                            close=false;
-                            open=true;
-                            fprintf(output_t,"Close number in a file of quotations %d %dy %dm %dd %dh close price= %f profit= %f\n",
-                                    i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_close[i],profit);
-
-                        }
+    int date_open=find_first_date(ui->tradeFrom->date()),date_close=find_last_date(ui->tradeTo->date());
+    for (int i=date_open;i<=date_close;i++){
+        mas_ao[i][0]=ao(vector,i);
+        mas_ao[i][1]=ao(vector,i-7);
+        mas_ao[i][2]=ao(vector,i-14);
+        mas_ao[i][3]=ao(vector,i-21);
+        //printf("a1= %f a2= %f a3= %f a4= %f i= %d\n",mas_ao[i][0],mas_ao[i][1],mas_ao[i][2],mas_ao[i][3],i);
     }
-    fclose(output_t);
-    printf("\nYou were trading from %dy %dm %dd %dh to %dy %dm %dd %dh\n",year_to,month_to,day_to,hour_to,year_tc,month_tc,day_tc,hour_tc);
-    printf("Trading complete.\n w1=%f  w2=%f w3=%f w4=%f profit=%f",w1_max,w2_max,w3_max,w4_max,profit);
-    printf("\n");//*/
+   //Торговля
+   FILE* output_t=fopen("output_t.txt","w+");
+   double profit=0,d=0;
+   bool open=true,close=false;
+   int c=0;
+   fprintf(output_t,
+           "==========================\nCoefficients: w1=%d w2=%d w3=%d w4=%d\n===========================\n",
+           ui->tradeW1->text().toInt(),ui->tradeW2->text().toInt(),ui->tradeW3->text().toInt(),ui->tradeW4->text().toInt());
+   for (int i=date_open;i<=date_close;i++){
+                       d=mas_ao[i][0]*(100-ui->tradeW1->text().toInt())+mas_ao[i][1]*(100-ui->tradeW2->text().toInt())+
+                               mas_ao[i][2]*(100-ui->tradeW3->text().toInt())+mas_ao[i][3]*(100-ui->tradeW4->text().toInt());
+                       if (open==true){
+                          if (d>0){
+                             c++;
+                             profit=profit-vector[i]->open();
+                             open=false;
+                             close=true;
+                             fprintf(output_t,
+                                     "--------------------------------------------\nDeal id= %d:\nOpen number in a file of quotations %d, date: %s %s, open price - %f profit= %f\n",
+                                     c,i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->open(),profit);
+                          }
+                       }
+                       if (close==true){
+                          if (d<0){
+                             profit=profit+vector[i]->close();
+                             close=false;
+                             open=true;
+                             fprintf(output_t,
+                                     "Close number in a file of quotations %d %s %s close price= %f profit= %f\n",
+                                     i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->close(),profit);
+                          }
+                       }
+                       if ((close==true)&&(i==date_close)){
+                           profit=profit+vector[i]->close();
+                           close=false;
+                           open=true;
+                           fprintf(output_t,
+                                   "Close number in a file of quotations %d %s %s close price= %f profit= %f\n",
+                                   i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->close(),profit);
+
+                       }
+   }
+   fclose(output_t);
+   ui->tradeProfit->setText(QString::number(profit));
 }
 
 void mainwindow::on_start_all_clicked()
 {
-    /*
-     //нахождение всех необходимы коефициетвов осцилляторов для обучения
-     for (int i=open_l;i<=close_l;i++){
-         mas_ao[i][0]=ao(mas_low,mas_high,i);
-         mas_ao[i][1]=ao(mas_low,mas_high,i-7);
-         mas_ao[i][2]=ao(mas_low,mas_high,i-14);
-         mas_ao[i][3]=ao(mas_low,mas_high,i-21);
-         //printf("a1= %f a2= %f a3= %f a4= %f i= %d\n",mas_ao[i][0],mas_ao[i][1],mas_ao[i][2],mas_ao[i][3],i);
-     }
+    /*//2000.02.12
+    QDate dt(2006,12,23);
+    qDebug()<<"Correct id:"<<43344<<"find date id:"<<find_date(dt)<<
+              "find first date id"<<find_first_date(dt)<<"find last date id"<<find_last_date(dt);//*/
 
-     float profit_max=0,w1_max,w2_max,w3_max,w4_max;
-    //обучение(подбор весовых коефициентов, при которых профит максимальный)
-    //FILE* output_l=fopen("output_l.txt","w+");
-    bool open=true,close=false;
-    for (int i1=1;i1<=100;i1++){
-        //ui->progressBar->setValue(i1-1);
-        for (int i2=1;i2<=100;i2++){
-            for (int i3=1;i3<=100;i3++){
-                for (int i4=1;i4<=100;i4++){
-                    //c=0;
-                    float profit=0;
-                    open=true;
-                    close=false;
-                    for (int i=open_l;i<=close_l;i++){
-                        float d=mas_ao[i][0]*i1+mas_ao[i][1]*i2+mas_ao[i][2]*i3+mas_ao[i][3]*i4;
-                        if (open==true){
-                           if (d>0){
-                              //c++;
-                              profit=profit-mas_open[i];
-                              open=false;
-                              close=true;
-                           }
-                        }
-                        if (close==true){
-                           if (d<0){
-                              profit=profit+mas_close[i];
-                              close=false;
-                              open=true;
-                           }
-                        }
-                        if ((close==true)&&(i==close_l)){
-                            profit=profit+mas_close[i];
-                            close=false;
-                            open=true;
-                        }
+    int date_open=find_first_date(ui->trainFrom->date()),date_close=find_last_date(ui->trainTo->date());
 
-                        if (profit>profit_max){
-                           profit_max=profit;
-                           w1_max=i1;
-                           w2_max=i2;
-                           w3_max=i3;
-                           w4_max=i4;
-                        }
-                    }
-                }
-            }
-        }
-    }//
-
-    //нахождение всех необходимы коефициетвов осцилляторов для торговли
-     for (i=open_t;i<=close_t;i++){
-         mas_ao[i][0]=ao(mas_low,mas_high,i);
-         mas_ao[i][1]=ao(mas_low,mas_high,i-7);
-         mas_ao[i][2]=ao(mas_low,mas_high,i-14);
-         mas_ao[i][3]=ao(mas_low,mas_high,i-21);
-         //printf("a1= %f a2= %f a3= %f a4= %f i= %d\n",mas_ao[i][0],mas_ao[i][1],mas_ao[i][2],mas_ao[i][3],i);
-     }
-    //Торговля
-    FILE* output_t=fopen("output_t.txt","w+");
-    float profit=0;
-    open=true,close=false;
-    int c=0;
-    fprintf(output_t,
-            "==========================\nCoefficients: w1=%f w2=%f w3=%f w4=%f\n===========================\n",
-            w1_max,w2_max,w3_max,w4_max);
-    for (int i=open_t;i<=close_t;i++){
-                        d=mas_ao[i][0]*w1_max+mas_ao[i][1]*w2_max+mas_ao[i][2]*w3_max+mas_ao[i][3]*w4_max;
-                        if (open==true){
-                           if (d>0){
-                              c++;
-                              profit=profit-mas_open[i];
-                              open=false;
-                              close=true;
-                              fprintf(output_t,
-                                      "--------------------------------------------\nDeal id= %d:\nOpen number in a file of quotations %d %dy %dm %dd %dh open price= %f profit= %f\n",
-                                      c,i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_open[i],profit);
-                           }
-                        }
-                        if (close==true){
-                           if (d<0){
-                              profit=profit+mas_close[i];
-                              close=false;
-                              open=true;
-                              fprintf(output_t,
-                                      "Close number in a file of quotations %d %dy %dm %dd %dh close price= %f profit= %f\n",
-                                      i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_close[i],profit);
-                           }
-                        }
-                        if ((close==true)&&(i==close_l)){
-                            profit=profit+mas_close[i];
-                            close=false;
-                            open=true;
-                            fprintf(output_t,"Close number in a file of quotations %d %dy %dm %dd %dh close price= %f profit= %f\n",
-                                    i,mas_year[i],mas_month[i],mas_day[i],mas_hour[i],mas_close[i],profit);
-
-                        }
+    //нахождение всех необходимы коефициетвов осцилляторов для обучения
+    for (int i=date_open;i<=date_close;i++){
+        mas_ao[i][0]=ao(vector,i);
+        mas_ao[i][1]=ao(vector,i-7);
+        mas_ao[i][2]=ao(vector,i-14);
+        mas_ao[i][3]=ao(vector,i-21);
     }
-    fclose(output_t);
-    printf("\nYou were trading from %dy %dm %dd %dh to %dy %dm %dd %dh\n",year_to,month_to,day_to,hour_to,year_tc,month_tc,day_tc,hour_tc);
-    printf("Trading complete.\n w1=%f  w2=%f w3=%f w4=%f profit=%f",w1_max,w2_max,w3_max,w4_max,profit);
-    printf("\n");//*/
+
+   double profit_max=0,w1_max=0,w2_max=0,w3_max=0,w4_max=0;
+   //обучение(подбор весовых коефициентов, при которых профит максимальный)
+   bool open=true,close=false;
+   for (int i1=1;i1<=100;i1++){
+       ui->progressBar->setValue(i1);
+       for (int i2=1;i2<=100;i2++){
+           for (int i3=1;i3<=100;i3++){
+               for (int i4=1;i4<=100;i4++){
+                   //c=0;
+                   float profit=0;
+                   open=true;
+                   close=false;
+                   for (int i=date_open;i<=date_close;i++){
+                       double d=mas_ao[i][0]*(i1-100)+mas_ao[i][1]*(i2-100)+mas_ao[i][2]*(i3-100)+mas_ao[i][3]*(i4-100);
+                       if (open){
+                          if (d>0){
+                             profit=profit-vector[i]->open();
+                             open=false;
+                             close=true;
+                          }
+                       }
+                       if (close){
+                          if (d<0){
+                             profit=profit+vector[i]->close();
+                             close=false;
+                             open=true;
+                          }
+                       }
+                       if ((close)&&(i==date_close)){
+                           profit=profit+vector[i]->close();
+                           close=false;
+                           open=true;
+                       }
+
+                       if (profit>profit_max){
+                          profit_max=profit;
+                          w1_max=i1;
+                          w2_max=i2;
+                          w3_max=i3;
+                          w4_max=i4;
+                          ui->trainProfit->setText(QString::number(profit_max));
+                          ui->trainW1->setText(QString::number(w1_max));
+                          ui->trainW2->setText(QString::number(w2_max));
+                          ui->trainW3->setText(QString::number(w3_max));
+                          ui->trainW4->setText(QString::number(w4_max));
+                       }
+                   }
+               }
+           }
+       }
+   }//*/
+   ui->trainProfit->setText(QString::number(profit_max));
+   ui->trainW1->setText(QString::number(w1_max));
+   ui->trainW2->setText(QString::number(w2_max));
+   ui->trainW3->setText(QString::number(w3_max));
+   ui->trainW4->setText(QString::number(w4_max));
+
+
+   ui->tradeW1->setText(QString::number(w1_max));
+   ui->tradeW2->setText(QString::number(w2_max));
+   ui->tradeW3->setText(QString::number(w3_max));
+   ui->tradeW4->setText(QString::number(w4_max));
+
+
+
+   date_open=find_first_date(ui->tradeFrom->date()),date_close=find_last_date(ui->tradeTo->date());
+   for (int i=date_open;i<=date_close;i++){
+       mas_ao[i][0]=ao(vector,i);
+       mas_ao[i][1]=ao(vector,i-7);
+       mas_ao[i][2]=ao(vector,i-14);
+       mas_ao[i][3]=ao(vector,i-21);
+       //printf("a1= %f a2= %f a3= %f a4= %f i= %d\n",mas_ao[i][0],mas_ao[i][1],mas_ao[i][2],mas_ao[i][3],i);
+   }
+  //Торговля
+  FILE* output_t=fopen("output_t.txt","w+");
+  double profit=0,d=0;
+  open=true,close=false;
+  int c=0;
+  fprintf(output_t,
+          "==========================\nCoefficients: w1=%d w2=%d w3=%d w4=%d\n===========================\n",
+          ui->tradeW1->text().toInt(),ui->tradeW2->text().toInt(),ui->tradeW3->text().toInt(),ui->tradeW4->text().toInt());
+  for (int i=date_open;i<=date_close;i++){
+                      d=mas_ao[i][0]*(100-ui->tradeW1->text().toInt())+mas_ao[i][1]*(100-ui->tradeW2->text().toInt())+
+                              mas_ao[i][2]*(100-ui->tradeW3->text().toInt())+mas_ao[i][3]*(100-ui->tradeW4->text().toInt());
+                      if (open==true){
+                         if (d>0){
+                            c++;
+                            profit=profit-vector[i]->open();
+                            open=false;
+                            close=true;
+                            fprintf(output_t,
+                                    "--------------------------------------------\nDeal id= %d:\nOpen number in a file of quotations %d, date: %s %s, open price - %f profit= %f\n",
+                                    c,i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->open(),profit);
+                         }
+                      }
+                      if (close==true){
+                         if (d<0){
+                            profit=profit+vector[i]->close();
+                            close=false;
+                            open=true;
+                            fprintf(output_t,
+                                    "Close number in a file of quotations %d %s %s close price= %f profit= %f\n",
+                                    i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->close(),profit);
+                         }
+                      }
+                      if ((close==true)&&(i==date_close)){
+                          profit=profit+vector[i]->close();
+                          close=false;
+                          open=true;
+                          fprintf(output_t,
+                                  "Close number in a file of quotations %d %s %s close price= %f profit= %f\n",
+                                  i,vector[i]->date().toString("yyyy.MM.dd").toAscii().data(),vector[i]->time().toString("hh:mm").toAscii().data(),vector[i]->close(),profit);
+
+                      }
+  }
+  fclose(output_t);
+  ui->tradeProfit->setText(QString::number(profit));
 
 }
